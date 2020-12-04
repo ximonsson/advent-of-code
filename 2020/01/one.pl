e6573dcb1c789e451ca56d137e42a49dd99f0667
Simon Simonsson <simonsson.simon@gmail.com>
2020-12-04 19:46:04 +0100

    first (should work, but doesn't?)

---
+++ 2020/01/one.pl
@@ -0,0 +1,23 @@
+/**
+ * 01. Repair Report
+ * https://adventofcode.com/2020/day/1
+ */
+
+% only append if it is an int
+readint(H, T, T) :- \+ number(H), !.
+readint(H, T, [H|T]) :- number(H).
+
+% reads all lines of a file
+read_report([]) :- at_end_of_stream.
+read_report([]) :- read_token(H), \+ number(H), !.
+%read_report(L) :- read_token(H), readint(H, T, L), read_report(T).
+read_report([H|T]) :- read_token(H), read_report(T).
+
+% read all lines into L from file F
+report(L, F) :- see(F), read_report(L), seen.
+
+% find the two expenses that add to 2020 and give their product.
+expenses(Z, Rep) :- member(X, Rep), member(Y, Rep), X \= Y, X+Y =:= 2020, Z is X*Y.
+
+% main function to read the input and find the value
+main :- report(L, 'input'), expenses(X, L), write(X), nl, !.
