5d468b3dc4a8c845c141d23542bc756ac4e9aa74
Simon Simonsson <simonsson.simon@gmail.com>
2020-12-04 23:45:18 +0100

    part two

--- 2020/01/one.pl
+++ 2020/01/one.pl
@@ -15,7 +15,16 @@ read_report(L) :- read_token(H), readint(H, T, L), read_report(T).
 report(L, F) :- see(F), read_report(L), seen.
 
 % find the two expenses that add to 2020 and give their product.
-expenses(Z, Rep) :- member(X, Rep), member(Y, Rep), X \= Y, X+Y =:= 2020, Z is X*Y.
+%expenses(Z, Rep) :- member(X, Rep), member(Y, Rep), X \= Y, X+Y =:= 2020, Z is X*Y.
+expenses(Z, Rep) :-
+	member(X, Rep),
+	member(Y, Rep),
+	member(S, Rep),
+	X \= Y,
+	X \= S,
+	S \= Y,
+	X+Y+S =:= 2020,
+	Z is X*Y*S.
 
 % main function to read the input and find the value
 main :- report(L, 'input'), expenses(X, L), write(X), nl, !.
