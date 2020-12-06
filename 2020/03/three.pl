46fb808eb5233219ea630df8309ae9e2421c7861
Simon Simonsson <simonsson.simon@gmail.com>
2020-12-06 19:13:58 +0100

    reading map

--- 2020/03/three.pl
+++ 2020/03/three.pl
@@ -2,4 +2,11 @@
 % Day 3: Toboggan Trajectory
 %
 
+read_row(R) :- read_token(T), atom_chars(T, R), get0(_).
 
+read_map([]) :- peek_code(C), C =:= -1, !.
+read_map([R|Rows]) :- read_row(R), read_map(Rows).
+
+read_map(F, M) :- see(F), read_map(M), seen.
+
+main :- read_map('input', M), write(M).
