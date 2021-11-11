31c01c37ee5152ce9d85e947408a829f622cdfe9
Simon Simonsson <simonsson.simon@gmail.com>
2021-11-11 23:21:23 +0100

    should push this so i don't lose it

--- 2020/03/three.pl
+++ 2020/03/three.pl
@@ -6,7 +6,9 @@ read_row(R) :- read_token(T), atom_chars(T, R), get0(_).
 
 read_map([]) :- peek_code(C), C =:= -1, !.
 read_map([R|Rows]) :- read_row(R), read_map(Rows).
-
 read_map(F, M) :- see(F), read_map(M), seen.
 
-main :- read_map('input', M), write(M).
+path([H|T], [R|M], I) :- nth(I, R, H), I is I + 3, path(T, M, I).
+path(P, M) :- path(P, M, 0).
+
+main :- read_map('input', M), path(P, M), write(P).
