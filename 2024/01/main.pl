068c49ce079d822ce3447c50ab4765acc5e74729
Simon Simonsson <simonsson.simon@gmail.com>
2024-12-29 18:55:07 +0100

    Day 1 complete

--- 2024/01/main.pl
+++ 2024/01/main.pl
@@ -1,6 +1,39 @@
-lines([X|T1], [Y|T2]) --> line(X, Y), '\n', lines(T1, T2).
+% https://adventofcode.com/2024/day/1
 
-line(X, Y) --> n(X), '   ', n(Y).
+% read file
 
-n(N) --> integer(N).
+add_loc(_, L, L) :- at_end_of_stream.
+add_loc(X, L, [X|L]).
 
+read_locs([]) :- at_end_of_stream.
+read_locs(L) :- read_token(H), add_loc(H, T, L), read_locs(T).
+
+locations(F, L) :- see(F), read_locs(L), seen.
+
+% separate list into two.
+
+ls([], [], []).
+ls([A|D], [B|E], [A, B|C]) :- ls(D, E, C).
+
+% part one
+
+% total distance.
+
+total_distance([], [], 0).
+total_distance([X|A], [Y|B], C) :-
+	D is abs(X - Y), total_distance(A, B, E), C is D + E.
+
+partone(F, D) :- locations(F, L), ls(X, Y, L), msort(X), msort(Y), total_distance(X, Y, D).
+
+% part two
+
+occurances(_, [], 0).
+occurances(X, [X|T], Z) :- occurances(X, T, Y), Z is 1 + Y.
+occurances(X, [_|T], Z) :- occurances(X, T, Z).
+
+similarity(X, Y, Z) :- occurances(X, Y, A), Z is X * A.
+
+similarity_score([], _, 0).
+similarity_score([H|T], X, Z) :- similarity(H, X, C), similarity_score(T, X, D), Z is C + D.
+
+parttwo(F, D) :- locations(F, L), ls(X, Y, L), similarity_score(X, Y, D).
