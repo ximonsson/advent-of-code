c53405da63947e1c8d94d78f14a35272af5cb159
Simon Simonsson <simonsson.simon@gmail.com>
2024-12-30 23:20:12 +0100

    Day 02 finished

--- 2024/02/main.pl
+++ 2024/02/main.pl
@@ -1,17 +1,59 @@
 % https://adventofcode.com/2024/day/2
-%
+
 % read file
 
-%add_loc(_, L, L) :- at_end_of_stream.
-%add_loc(X, L, [X|L]).
+read_input([]) :- at_end_of_stream, !.
+read_input([H|T]) :- get_char(H), read_input(T).
+read_input(F, In) :- see(F), read_input(In), seen.
 
-%read_locs([]) :- at_end_of_stream.
-%read_locs(L) :- read_token(H), add_loc(H, T, L), read_locs(T).
 
-read_line([]) :- at_end_of_stream, !.
-read_line([H|T]) :- get(H), read_line(T).
+% grammar for parsing
 
-reports(F, L) :- see(F), read_line(L), seen.
+reports([]) --> [].
+reports([R|Rs]) --> levels(R), ['\n'], reports(Rs).
 
-report --> levels.
+levels([]) --> [].
+levels([L|Ls]) --> level(L), [' '], levels(Ls).
+levels([L|[]]) --> level(L).
 
+level(L) --> digit(D0), digits(D), { number_chars(L, [D0|D]) }.
+
+digits([]) --> [].
+digits([D|T]) --> digit(D), !, digits(T).
+
+digit(D) --> [D], { char_code(D, C), between(48, 57, C) }.
+
+
+% valid report
+
+dir([_|[]], _) :- !.
+dir([A, B|T], Prev) :- X is sign(B - A), X =:= Prev, dir([B|T], X).
+dir([A, B|T]) :- X is sign(B - A), dir([B|T], X).
+
+step_size([_|[]]) :- !.
+step_size([A, B|T]) :- X is abs(A - B), between(1, 3, X), step_size([B|T]).
+
+valid(R) :- dir(R), step_size(R).
+
+filter_reports([], []) :- !.
+filter_reports([R|Rs], [R|Vs]) :- valid(R), filter_reports(Rs, Vs).
+filter_reports([R|Rs], Vs) :- \+ valid(R), filter_reports(Rs, Vs).
+
+
+% part one
+
+partone(F, N) :- read_input(F, Data), reports(Rs, Data, []), filter_reports(Rs, Vs), length(Vs, N).
+
+
+% dampener
+
+damp(H, _, T) :- append(H, T, R), valid(R), !.
+damp(H0, X, [H|T]) :- append(H0, [H|T], R), \+ valid(R), append(H0, [X], H1), damp(H1, H, T).
+damp([H|T]) :- damp([], H, T).
+
+filter_damp_reports([], []) :- !.
+filter_damp_reports([R|Rs], [R|Vs]) :- valid(R), filter_damp_reports(Rs, Vs).
+filter_damp_reports([R|Rs], [R|Vs]) :- \+ valid(R), damp(R), filter_damp_reports(Rs, Vs).
+filter_damp_reports([_|Rs], Vs) :- filter_damp_reports(Rs, Vs).
+
+parttwo(F, N) :- read_input(F, Data), reports(Rs, Data, []), filter_damp_reports(Rs, Vs), length(Vs, N).
