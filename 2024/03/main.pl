a400bd20c9790cfd2bce04fa7c48cca3616745d6
Simon Simonsson <simonsson.simon@gmail.com>
2025-01-01 22:26:08 +0100

    Day 03 Part II

--- 2024/03/main.pl
+++ 2024/03/main.pl
@@ -18,5 +18,27 @@ prod(mul(X, Y), Z) :- Z is X * Y.
 prod_list([H|T], [X|Y]) :- prod(H, X), prod_list(T, Y).
 prod_list([], []).
 
-% part two
+mul_it_over(F, N) :-
+	read_input(F, Data), phrase(muls(Muls), Data), prod_list(Muls, P), sum_list(P, N).
 
+% part two
+% ---
+% we solve it by removing everything that is between "don't()....do()", and then
+% run the solution from part one on the result.
+
+dos(X) --> do(Y), "don't()", dont, !, dos(Z), { append(Y, Z, X) }.
+dos(X) --> do(X).
+
+do([]) --> [].
+do([H|T]) --> [H], do(T).
+
+dont --> "do()".
+dont --> [_], dont.
+dont --> [].
+
+mul_it_over_2(F, N) :-
+	read_input(F, Data),
+	phrase(dos(X), Data),
+	phrase(muls(Muls), X),
+	prod_list(Muls, P),
+	sum_list(P, N).
