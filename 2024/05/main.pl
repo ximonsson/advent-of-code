0504f08e2b8d37b55dcd7a710dfdba4275302dc7
Simon Simonsson <simonsson.simon@gmail.com>
2025-01-05 10:08:26 +0100

    Day 04 Grammar for parsing file

--- 2024/05/main.pl
+++ 2024/05/main.pl
@@ -1,10 +1,23 @@
 :- [utils].
 
+% grammar
+% ---
 
-rules([R|Rs]) --> rule_(R), "\n", rules(Rs).
-rules([]) --> "\n".
+% rules
+
+rules([R|Rs]) --> rule_(R), "\n", !, rules(Rs).
+rules([]) --> [].
 
 rule_(rule(B, A)) --> integer(B), "|", integer(A).
 
-updates([U|Us]) --> update(U), "\n", updates(Us).
+% updates
 
+updates([U|Us]) --> update(U), "\n", !, updates(Us).
+updates([]) --> [].
+
+update([P|Ps]) --> integer(P), ",", update(Ps).
+update([P]) --> integer(P).
+
+% entire file
+
+inputfile(R, U) --> rules(R), "\n", updates(U).
