4ca0bc0b6db98fce9bc3bd4bb5a6cb81e7e8eb43
Simon Simonsson <simonsson.simon@gmail.com>
2025-01-05 08:28:43 +0100

    Day 04 Part II complete

--- 2024/04/main.pl
+++ 2024/04/main.pl
@@ -32,7 +32,6 @@ add_nl(L, L1) :- append(L, "\n", L1).
 % solution part one
 
 ceres_search(F, N) :- read_file_to_codes(F, Data, [access(read)]),
-%reverse(Data, Atad),
 	phrase(lines(Ls), Data),
 	% horizontal
 	xmas(Data, X0), samx(Data, X1),
@@ -42,11 +41,26 @@ ceres_search(F, N) :- read_file_to_codes(F, Data, [access(read)]),
 	xmas(DataT, X2), samx(DataT, X3),
 	% diagonal...
 	findall(_, diag(Ls), Dia), length(Dia, X4),
-
-	writeln(X0),
-	writeln(X1),
-	writeln(X2),
-	writeln(X3),
-	writeln(X4),
-
+	% sum
 	N is X0 + X1 + X2 + X3 + X4.
+
+
+% part two
+% - similar solution as the diagonal.
+
+x_mas_([
+	[A, _, D|_],
+	[_, B, _|_],
+	[E, _, C|_]
+]) :-
+	([A, B, C] == "MAS"; [A, B, C] == "SAM"),
+	([D, B, E] == "MAS"; [D, B, E] == "SAM").
+
+x_mas_([[_|T0], [_|T1], [_|T2]]) :- x_mas_([T0, T1, T2]).
+
+% scroll through lines
+x_mas([L1, L2, L3|_]) :- x_mas_([L1, L2, L3]).
+x_mas([_|T]) :- x_mas(T).
+
+ceres_search_2(F, N) :-
+	phrase_from_file(lines(Ls), F), findall(_, x_mas(Ls), X), length(X, N).
