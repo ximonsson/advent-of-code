3366389b4592a2c687e130fad59a29a9d2412070
Simon Simonsson <simonsson.simon@gmail.com>
2025-01-09 20:10:23 +0100

    Trying to solve Day 06 Part II
    Not working.

--- 2024/06/main.pl
+++ 2024/06/main.pl
@@ -31,6 +31,9 @@ rotcc(M, M1) :-
 rotc(M, M1) :-
 	map_codes_list(M, Ls), transpose(Ls, Ls0), reverse(Ls0, Ls1), map_codes_list(M1, Ls1).
 
+rotn(0, M, M).
+rotn(N, M, M0) :- N > 0, N0 is N - 1, rotc(M, M1), rotn(N0, M1, M0).
+
 % we are in a loop when the entire path has been walked already.
 
 obstacle("\n") --> "\n".
@@ -53,27 +56,32 @@ outside(M) :- \+ memberchk(62, M).
 write_map(M) :- string_codes(S, M), writeln(S).
 
 % we are in a loop
-patrol(M, 0, true, M) :- phrase(loop, M), !.
+patrol(M, 0, true, M, Moves, Moves) :- phrase(loop, M), !.
 
 % guard is not on the map, i.e. walked out
-patrol(M, N, false, M) :- outside(M), findall(_, phrase(match("X"), M), X), length(X, N).
+patrol(M, N, false, M, Moves, Moves) :- outside(M), findall(_, phrase(match("X"), M), X), length(X, N).
 
 % guard walks
-patrol(Map, N, Loop, Solution) :- phrase(walk(M1), Map), rotc(M1, M2), patrol(M2, N, Loop, Solution).
+patrol(Map, N, Loop, Solution, Acc, Moves) :-
+	phrase(walk(M1), Map), rotc(M1, M2), Acc0 is Acc + 1, patrol(M2, N, Loop, Solution, Acc0, Moves).
 
 make_loop(M, Ml) :-
+	nth0(I, M, 62),  % starting position for the guard
 	% first solve the puzzle. then only try putting obstacles in the path of the guard.
-	patrol(M, _, false, M1), replace_all(88, 120, M1, M2),
+	patrol(M, _, false, M1, 0, Moves), replace_all(88, 120, M1, M2),
 
 	% TODO
 	% place the guard in its original position.
+	rotn(4 - Moves mod 4, M2, M3),
+	replace0(I, 62, M3, M4),
 
 	% add the obstacle and try to solve and see if the guard gets into a loop.
-	char_code('O', C), select(120, M2, C, M0), patrol(M0, 0, true, Ml).
+	char_code('O', C), select(120, M4, C, M0), patrol(M0, 0, true, Ml, 0, _).
 
 init(M, M1) :- select(94, M, 62, M0), rotcc(M0, M1).
 
-guard_gallivant(F, N) :- read_file_to_codes(F, M, []), init(M, Map), patrol(Map, N, false, M2), write_map(M2).
+guard_gallivant(F, N) :-
+	read_file_to_codes(F, M, []), init(M, Map), patrol(Map, N, false, M2, 0, _), write_map(M2).
 
 guard_gallivant_2(F, N) :-
 	read_file_to_codes(F, M, []), init(M, Map), findall(_, make_loop(Map, _), X), length(X, N).
--- utils.pl
+++ utils.pl
@@ -75,3 +75,6 @@ replace0(I0, I1, E, X, Y) :-
 replace_all(_, _, [], []).
 replace_all(E, Enew, [E|T0], [Enew|T1]) :- replace_all(E, Enew, T0, T1).
 replace_all(E, Enew, [H|T0], [H|T1]) :- E \== H, replace_all(E, Enew, T0, T1).
+
+add([], [], []).
+add([H0|T0], [H1|T1], [H2|T2]) :- H2 is H0 + H1, add(T0, T1, T2).
